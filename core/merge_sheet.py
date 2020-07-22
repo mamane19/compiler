9e11e7f482231677bf3f77b391be0348d1f7a903
Boris Kayi <boriskayienzo@gmail.com>
2020-07-22 10:02:56 +0200

    Added code formatter & .gitignore

--- core/merge_sheet.py
+++ core/merge_sheet.py
@@ -2,16 +2,16 @@ import pandas as pd
 from os import path
 
 current_dir = path.dirname(path.abspath(__file__))
-excel_sheet1_path = path.join(current_dir, 'sheet1.xlsx')
-excel_sheet2_path = path.join(current_dir, 'sheet2.xlsx')
+excel_sheet1_path = path.join(current_dir, "sheet1.xlsx")
+excel_sheet2_path = path.join(current_dir, "sheet2.xlsx")
 
 sheet1_df = pd.read_excel(excel_sheet1_path)
 sheet2_df = pd.read_excel(excel_sheet2_path)
 merge = pd.merge(sheet1_df, sheet2_df, on="NAME")
-print(f'{merge}\n--------------')
+print(f"{merge}\n--------------")
 
-csv_sheet1_path = path.join(current_dir, 'music-data1.csv')
-csv_sheet2_path = path.join(current_dir, 'music-data2.csv')
+csv_sheet1_path = path.join(current_dir, "music-data1.csv")
+csv_sheet2_path = path.join(current_dir, "music-data2.csv")
 
 sheet1_df = pd.read_csv(csv_sheet1_path)
 sheet2_df = pd.read_csv(csv_sheet2_path)
